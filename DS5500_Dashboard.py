d0d8f9301084543265ae0641fc4288fe9521276b
cdatin1 <67667808+cdatin1@users.noreply.github.com>
2021-06-22 18:51:12 -0400

    Update DS5500_Dashboard.py

--- DS5500_Dashboard.py
+++ DS5500_Dashboard.py
@@ -10,14 +10,14 @@ import streamlit as st
 
 # read sentiment csv file in to a dataframe
 survey_comments = pd.read_csv('final_df.csv')
-print(survey_comments.head())
+#print(survey_comments.head())
 
 # convert StartDate column to be recognized as a date
 survey_comments['StartDate'] = pd.to_datetime(survey_comments['StartDate'])
 
 # create a more generalized date field for digestability of the dashboard
-survey_comments['month'] = survey_comments['StartDate'].dt.strftime('%m %Y')
-print(survey_comments.head())
+survey_comments['month'] = survey_comments['StartDate'].dt.strftime('%B %Y')
+#print(survey_comments.head())
 # Streamlit documentation: https://docs.streamlit.io/en/stable/getting_started.html
 
 # dashboard header and authors
@@ -34,12 +34,12 @@ st.write("""
 # aggregate to get total reviews by month
 survey_comments['constant'] = 1
 responses_by_month = survey_comments.groupby(['month']).sum()['constant'].to_frame().reset_index()
-print(responses_by_month.head())
+#print(responses_by_month.head())
 
 # aggregate to get % positive reviews by month
 positive_condition = survey_comments['compound_sentiment'] == 'positive'
 positive_responses = survey_comments[positive_condition].groupby(['month']).sum()['constant'].to_frame().reset_index()
-print(positive_responses.head())
+#print(positive_responses.head())
 
 # merge dataframes together and calculate percentage
 responses_by_month = pd.merge(responses_by_month, positive_responses, on='month')
@@ -47,7 +47,18 @@ responses_by_month.columns = ['month', 'total_responses', 'positive_responses']
 responses_by_month['percent_positive'] = responses_by_month['positive_responses']/responses_by_month['total_responses']
 print(responses_by_month.head())
 
+# format month column so it's aligned properly in the chart
+responses_by_month['month'] = pd.to_datetime(responses_by_month['month'])
+responses_by_month = responses_by_month.sort_values(by='month')
+
+print(responses_by_month.head())
+
+responses_by_month.to_csv('test.csv')
+#responses_by_month.index = monthly_transpose.index.strftime('%m %Y')
+
+
 # graph bar chart
+st.write("Pertentage of Positive Reviews by Month")
 total_time_bar = st.line_chart(responses_by_month[['month', 'percent_positive']].set_index('month'))
 
 
@@ -55,42 +66,41 @@ st.write("""
 ## Monthly Analysis""")
 
 # drop down menu to select timeframe for filtering data
-month_list = ['08 2018',
-              '09 2018',
-              '10 2018',
-              '11 2018',
-              '12 2018',
-              '01 2019',
-              '02 2019',
-              '03 2019',
-              '04 2019',
-              '05 2019',
-              '06 2019',
-              '07 2019',
-              '08 2019',
-              '09 2019',
-              '10 2019',
-              '11 2019',
-              '12 2019',
-              '01 2020',
-              '02 2020',
-              '03 2020',
-              '04 2020',
-              '05 2020',
-              '06 2020',
-              '07 2020',
-              '08 2020',
-              '09 2020',
-              '10 2020',
-              '11 2020',
-              '12 2020']
+month_list = ['August 2018',
+              'September 2018',
+              'October 2018',
+              'November 2018',
+              'December 2018',
+              'January 2019',
+              'February 2019',
+              'March 2019',
+              'April 2019',
+              'May 2019',
+              'June 2019',
+              'July 2019',
+              'August 2019',
+              'September 2019',
+              'October 2019',
+              'November 2019',
+              'December 2019',
+              'January 2020',
+              'February 2020',
+              'March 2020',
+              'April 2020',
+              'May 2020',
+              'June 2020',
+              'July 2020',
+              'August 2020',
+              'September 2020',
+              'October 2020',
+              'November 2020',
+              'December 2020']
 
 month_year_option = st.selectbox('Select a month to filter the Dashboard:', month_list)
-'Month selected: ', month_year_option
+#'Month selected: ', month_year_option
 print(month_year_option)
 print(type(month_year_option))
 
-test = 'Aug 2018'
 
 date_requirement = survey_comments['month'] == month_year_option
 
@@ -101,6 +111,7 @@ date_filtered_survey = survey_comments[date_requirement]
 # aggregate data by sentiment and create a bar chart
 date_filtered_survey['constant'] = 1
 sentiment_plot = date_filtered_survey.groupby(['compound_sentiment']).sum()['constant'].to_frame()
+st.write('Month Survey Volume by Sentiment')
 st.bar_chart(sentiment_plot)
 
 # remove unnessecary columns and display dataframe
